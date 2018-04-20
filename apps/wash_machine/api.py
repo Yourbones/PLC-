82b313b8b57669fbd13d457c3f946f7308f48414
Yourbones <xhx@taipuauto.cn>
2018-04-20 15:28:04 +0800

    添加日志下载API

--- apps/wash_machine/api.py
+++ apps/wash_machine/api.py
@@ -4,6 +4,10 @@ from __future__ import unicode_literals
 
 import logging, json, time, threading
 
+import os, zipfile
+from django.http import StreamingHttpResponse
+from rest_framework.permissions import AllowAny
+
 from apps.wash_machine.models import OrderRecord
 from common.permissions import IsTpServerUser
 
@@ -18,6 +22,50 @@ class WashMachineResetViewSet(APIView):
 
     def post(self, request, *args, **kwargs):
         pass
+    
+# 下载日志文件
+class DownloadLogsViewSet(APIView):
+    """
+    下载工控机日志文件
+    """
+    permission_classes = (AllowAny,)
+    def get(self, request, *args, **kwargs):
+
+        # 定义压缩文件夹函数
+        def make_zip(file_name, output_file):
+            z = zipfile.ZipFile(output_file, 'w', zipfile.ZIP_DEFLATED)
+            pre_len = len(os.path.dirname(file_name))
+            for dirpath, dirnames, filenames in os.walk(file_name):
+                for filename in filenames:
+                    file_path = os.path.join(dirpath, filename)
+                    arcname = file_path[pre_len:].strip(os.path.sep)
+                    z.write(file_path, arcname)
+            z.close()
+
+        # 编写迭代器，处理文件
+        def file_iterator(fn, chunk_size=512):
+            while True:
+                c = fn.read(chunk_size)
+                if c:
+                    yield c
+                else:
+                    break
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        file_name = os.path.join(BASE_DIR, + '\\logs')
+        output_file = file_name + '.zip'
+        make_zip(file_name, output_file)
+        fn = open(output_file, 'rb')
+        response = StreamingHttpResponse(file_iterator(fn))
+        response['Content-Type'] = 'application/octet-stream'
+        response['Content-Disposition'] = 'attachment;filename = "logs.zip"'
+
+        return response
+
+
+
+
+
+
 
 
 
