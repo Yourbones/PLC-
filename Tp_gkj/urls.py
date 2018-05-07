7d4d05443778c5f1d3fa3c1fa1a44f99ffbf2a39
Yourbones <xhx@taipuauto.cn>
2018-05-07 10:18:22 +0800

    更新urls.py

--- Tp_gkj/urls.py
+++ Tp_gkj/urls.py
@@ -18,7 +18,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
-from apps.wash_machine.api import DownloadLogsViewSet
+from apps.wash_machine.api import WashMachineInfoViewSet, WashMachineStartViewSet, \
+    WashMachineStopViewSet, WashMachineResetViewSet,DownloadLogsViewSet
 
 if settings.DEBUG:
     router = DefaultRouter(schema_title='Pastebin API', schema_url='api')
@@ -34,5 +35,9 @@ urlpatterns = [
 ]
 
 urlpatterns += [
+    url(r'^info$', WashMachineInfoViewSet.as_view(), name='wash_machine_info'),
+    url(r'^start$', WashMachineStartViewSet.as_view(), name='wash_machine_start'),
+    url(r'^stop$', WashMachineStopViewSet.as_view(), name='wash_machine_stop'),
+    url(r'^reset$', WashMachineResetViewSet.as_view(), name='wash_machine_reset'),
     url(r'^download$', DownloadLogsViewSet.as_view(), name='download_logs')
 ]
